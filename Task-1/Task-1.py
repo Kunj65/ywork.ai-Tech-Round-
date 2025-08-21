9d1415598c2feec16c0f6e20fa9cd431fc7100c3
Kunj Sherathiya <177952870+Kunj65@users.noreply.github.com>
2025-08-21 16:20:26 +0530

    Task-1

---
+++ Task-1/Task-1.py
@@ -0,0 +1,30 @@
+board =[["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],
+["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],
+[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]]
+if board: 
+    def is_valid(board, row, col, num):
+        for i in range(9):
+            if board[row][i] == num or board[i][col] == num:
+                return False
+        start_row, start_col = 3 * (row // 3), 3 * (col // 3)
+        for i in range(start_row, start_row + 3):
+            for j in range(start_col, start_col + 3):
+                if board[i][j] == num:
+                    return False
+        return True
+
+    def solve_sudoku(board):
+        for i in range(9):
+            for j in range(9):
+                if board[i][j] == '.':
+                    for num in map(str, range(1, 10)):
+                        if is_valid(board, i, j, num):
+                            board[i][j] = num
+                            if solve_sudoku(board):
+                                return True
+                            board[i][j] = '.'
+                    return False
+        return True
+
+    solve_sudoku(board)
+    print(board)
